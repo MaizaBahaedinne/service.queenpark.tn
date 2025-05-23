08d920089c4253367c0db9c962d6fe455c05e798
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-23 21:36:11 +0200

    Update dashboard.php

--- application/views/general/dashboard.php
+++ application/views/general/dashboard.php
@@ -6,9 +6,9 @@
         <small>Queen Park Services</small>
       </h1>
     </section>
-    
+     <!--
     <section class="content">
-     <!--   <div class="row">
+       <div class="row">
             <div class="col-lg-3 col-xs-6">
              
               <div class="small-box bg-aqua">
@@ -61,8 +61,8 @@
                 <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div>
-          </div>  -->
-    </section>
+          </div>  
+    </section>-->
     <section class="content">
      <div class="row">
             <div class="col-xs-12">
