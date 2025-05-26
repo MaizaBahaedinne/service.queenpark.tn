8df23307ccd238d56fdf36516b8b10345addd355
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-26 11:46:35 +0200

    Update sortie.php

--- application/views/service/sortie.php
+++ application/views/service/sortie.php
@@ -65,6 +65,17 @@
     <div class="row">
       <div class="col-lg-12 col-xs-12">
         <h3>Retours après consommation</h3>
+              <?php if ($this->session->flashdata('success')): ?>
+        <div class="alert alert-success">
+          <?= $this->session->flashdata('success') ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($this->session->flashdata('error')): ?>
+        <div class="alert alert-danger">
+          <?= $this->session->flashdata('error') ?>
+        </div>
+      <?php endif; ?>
 
         <form method="post" action="<?php echo base_url() ?>Reservation/addRetours/<?php echo $reservation->reservationId ?>" class="form-style">
           <?php foreach ($entrees as $entree) : ?>
