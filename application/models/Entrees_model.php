c97eabbf69590dec0631613ed0f11b44c398751e
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-26 21:30:43 +0200

    Update Entrees_model.php

--- application/models/Entrees_model.php
+++ application/models/Entrees_model.php
@@ -12,21 +12,33 @@ class Entrees_model extends CI_Model {
 
     protected $table = 'tbl_services_entrees';
 
-    public function get_all() {
-        $query = $this->db->get($this->table);
-        $result = [];
+   public function get_all() {
+            $query = $this->db->get($this->table);
+            $result = [];
 
-        foreach ($query->result() as $row) {
-            $result[] = [
-                'id'    => $row->entreeId,
-                'title' => $row->nature . ' x' . $row->quantite,
-                'start' => $row->heurePrevu,
-                'end'   => date('Y-m-d H:i:s', strtotime($row->heurePrevu) + 1800),
-            ];
+            foreach ($query->result() as $row) {
+                // On ignore les entrées sans heure prévue
+                if (!empty($row->heurePrevu)) {
+                    $start = $row->heurePrevu;
+                    $timestamp = strtotime($start);
+
+                    // Juste au cas où strtotime échoue, on vérifie
+                    if ($timestamp !== false) {
+                        $end = date('Y-m-d H:i:s', $timestamp + 1800); // +30 min
+
+                        $result[] = [
+                            'id'    => $row->entreeId,
+                            'title' => $row->nature . ' x' . $row->quantite,
+                            'start' => $start,
+                            'end'   => $end,
+                        ];
+                    }
+                }
+            }
+
+            return $result;
         }
 
-        return $result;
-    }
 
     public function update_heure_prevu($id, $new_start) {
         return $this->db->where('entreeId', $id)
