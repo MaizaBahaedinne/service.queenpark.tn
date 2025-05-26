5f21c0c1592ff55cda9d5f79494425c290689e9c
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-26 11:43:15 +0200

    Update home.php

--- application/views/service/home.php
+++ application/views/service/home.php
@@ -9,7 +9,7 @@
      
     <section class="content">
        <div class="row">
-            <div class="col-lg-3 col-xs-6">
+            <div class="col-lg-4 col-xs-6">
              <a href="<?php echo base_url() ?>Reservation/entree/<?php echo $reservationId ?>"  class="small-box bg-aqua" >
               <div >
                 <div class="inner">
@@ -24,7 +24,7 @@
               </a>
             </div>
 
-             <div class="col-lg-3 col-xs-6">
+             <div class="col-lg-4 col-xs-6">
              <a href="<?php echo base_url() ?>Reservation/sortie/<?php echo $reservationId ?>"  class="small-box bg-orange" >
               <div >
                 <div class="inner">
@@ -38,6 +38,21 @@
               </div>
               </a>
             </div>
+
+            <div class="col-lg-4 col-xs-6">
+             <a href="<?php echo base_url() ?>Reservation/satisfaction/<?php echo $reservationId ?>"  class="small-box bg-red" >
+              <div >
+                <div class="inner">
+                  <h3 style="color: white;">Cloture</h3>
+                  <p></p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-happy"></i>
+                </div>
+                <a href="<?php echo base_url() ?>Reservation/sotie/<?php echo $reservationId ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+              </a>
+            </div>           
             
            
           </div>  
