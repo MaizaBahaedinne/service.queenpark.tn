75420c38608a1ad31186ee325a09ab40550f7cdd
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-30 17:29:40 +0200

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -40,7 +40,7 @@ class User extends BaseController
 
         $data['userRecords'] = $this->reservation_model->ReservationAffecationListing($this->vendorId); 
         
-          if ($this->role == 2 ) {   $data['userRecords'] = $this->reservation_model->ReservationListing() ;  }
+          if ($this->vendorId == 25 || $this->vendorId == 1 ) {   $data['userRecords'] = $this->reservation_model->ReservationListing() ;  }
 
         $this->loadViews("general/dashboard", $this->global, $data , NULL);
     }
